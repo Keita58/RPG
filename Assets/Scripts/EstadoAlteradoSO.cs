73a279f088a3acb60002dd653409a2455d3701bc
Keita58 <marc-s-26@hotmail.com>
2024-11-13 20:24:09 +0100

    aaaaaaaaaaaaa

---
+++ Assets/Scripts/EstadoAlteradoSO.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EstadoAlteradoSO", menuName = "Scriptable Objects/EstadoAlteradoSO")]
+public class EstadoAlteradoSO : ScriptableObject
+{
+    /**
+     * Incapacitat -> Depenent del número
+     */
+    public int incapacitat;
+    public int torns;
+    public int hp;
+    public int modAtk;
+    public int modDef;
+    public int modSpd;
+    public string nom;
+}
