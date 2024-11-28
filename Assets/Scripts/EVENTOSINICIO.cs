519d571fca6115275add9d361deac27c604a50b3
Hector Lara Garcia <e.hlarag@ies-sabadell.cat>
2024-11-28 18:32:18 +0100

    AIDJWIOADHWOA

---
+++ Assets/Scripts/EVENTOSINICIO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class EVENTOSINICIO : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Sortir()
+    {
+        Application.Quit();
+    }
+
+  
+}
