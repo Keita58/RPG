26e02bc099a33a3175bea2897fc5b985854c6f89
Keita58 <marc-s-26@hotmail.com>
2024-12-11 17:28:47 +0100

    Audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,16 +2,54 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    [Header("Player")]
+    [Header("Jugador")]
     [SerializeField] private AudioSource _Atac;
     [SerializeField] private AudioSource _Atac2;
+    [SerializeField] private AudioSource _Mal;
     [SerializeField] private AudioSource _Mort;
 
-    [Header("Enemy")]
-    [SerializeField] private AudioSource _AtacEnemic;
-    [SerializeField] private AudioSource _AtacEnemic2;
+    [Header("Enemic-Espadatxi")]
+    [SerializeField] private AudioSource _AtacEspadatxi;
+    [SerializeField] private AudioSource _AtacEspadatxi2;
+    [SerializeField] private AudioSource _AtacEspadatxi3;
+    [SerializeField] private AudioSource _MalEspadatxi;
+    [SerializeField] private AudioSource _MortEspadatxi;
 
-    [Header("Music")]
+    [Header("Enemic-Goblin")]
+    [SerializeField] private AudioSource _AtacGoblin;
+    [SerializeField] private AudioSource _AtacGoblin2;
+    [SerializeField] private AudioSource _AtacGoblin3;
+    [SerializeField] private AudioSource _MalGoblin;
+    [SerializeField] private AudioSource _MortGoblin;
+
+    [Header("Enemic-Mac")]
+    [SerializeField] private AudioSource _AtacMac;
+    [SerializeField] private AudioSource _AtacMac2;
+    [SerializeField] private AudioSource _AtacMac3;
+    [SerializeField] private AudioSource _MalMac;
+    [SerializeField] private AudioSource _MortMac;
+
+    [Header("Musica")]
     [SerializeField] private AudioSource _OWMusica;
     [SerializeField] private AudioSource _ArenaMusica;
+
+    public void AtacJugador()
+    {
+        _Atac.Play();
+    }
+
+    public void AtacEspadatxi()
+    {
+
+    }
+
+    public void AtacGoblin()
+    {
+
+    }
+
+    public void AtacMac()
+    {
+
+    }
 }
